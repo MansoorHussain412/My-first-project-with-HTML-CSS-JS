60c0015ffa3c4cdd712938e08740200596298965
MansoorHussain412 <hussainmansoor55555@gmail.com>
2025-11-07 04:05:49 -1200

    Add files via upload

---
+++ 1st.js
@@ -0,0 +1,29 @@
+
+let div = document.querySelector("div");
+div.style.textAlign = "center";
+div.style.marginTop = "50px";
+div.style.fontFamily = "Arial, sans-serif";
+div.style.height = "390px";
+div.style.backgroundColor = "white";
+
+let button = document.querySelector("button");;
+button.style.padding = "10px 20px";
+button.style.fontSize = "16px";
+button.style.cursor = "pointer";
+button.style.backgroundColor = "#4CAF50";
+button.style.color = "white";
+button.style.border = "none";
+button.style.borderRadius = "5px";
+button.style.marginTop = "20px";
+button.style.boxShadow = "0 4px #999";
+button.style.transition = "background-color 0.3s";
+button.style.display = "..."
+
+button.addEventListener("click", function() {
+    alert("Hello, I am Mansoor Hussain, learning HTML, CSS, and JS!");
+});
+
+
+
+
+
